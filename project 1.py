0959070b6875d56a420cd3e7ca74abe173783272
Jhoan Stiven <128644723+SuperJhoan@users.noreply.github.com>
2024-10-20 17:52:02 -0500

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <nav class="navbar">
+      <h1>Breathe Code Photo Feed</h1>
+    </nav>
+
+    <section class="insta-post">
+      <div class="post-container">
+        <div class="post-header">
+          <div class="post-title-container">
+            <span class="post-title">My First Photo</span>
+          </div>
+          <div class="post-date-container">
+            <div class="post-date">12/14</div>
+          </div>
+        </div>
+        <div class="post-photo-container">
+          <div class="post-photo">
+            <img src="https://picsum.photos/400" alt="photo" />
+          </div>
+        </div>
+        <div class="post-footer-container">
+          <div class="post-footer">
+            <span class="post-footer-description"
+              >This is my first photo to feed, this is a multi line comment for
+              testing purposes. I am learning to code with
+              <span class="inside-span">#BreatheCodeByJhoan</span></span
+            >
+          </div>
+        </div>
+      </div>
+      <div class="post-container">
+        <div class="post-header">
+          <div class="post-title-container">
+            <span class="post-title">My Second Photo</span>
+          </div>
+          <div class="post-date-container">
+            <div class="post-date">12/14</div>
+          </div>
+        </div>
+        <div class="post-photo-container">
+          <div class="post-photo">
+            <img src="https://picsum.photos/id/237/400" alt="photo" />
+          </div>
+        </div>
+        <div class="post-footer-container">
+          <div class="post-footer">
+            <span class="post-footer-description"
+              >This is my first photo to feed, this is a multi line comment for
+              testing purposes. I am learning to code with
+              <span class="inside-span">#BreatheCodeByJhoan</span></span
+            >
+          </div>
+        </div>
+      </div>
+      <div class="post-container">
+        <div class="post-header">
+          <div class="post-title-container">
+            <span class="post-title">My Third Photo</span>
+          </div>
+          <div class="post-date-container">
+            <div class="post-date">12/14</div>
+          </div>
+        </div>
+        <div class="post-photo-container">
+          <div class="post-photo">
+            <img src="https://picsum.photos/id/238/400" alt="photo" />
+          </div>
+        </div>
+        <div class="post-footer-container">
+          <div class="post-footer">
+            <span class="post-footer-description"
+              >This is my first photo to feed, this is a multi line comment for
+              testing purposes. I am learning to code with
+              <span class="inside-span">#BreatheCodeByJhoan</span></span
+            >
+          </div>
+        </div>
+      </div>
+      <div class="post-container">
+        <div class="post-header">
+          <div class="post-title-container">
+            <span class="post-title">My Fourth Photo</span>
+          </div>
+          <div class="post-date-container">
+            <div class="post-date">12/14</div>
+          </div>
+        </div>
+        <div class="post-photo-container">
+          <div class="post-photo">
+            <img src="https://picsum.photos/id/239/400" alt="photo" />
+          </div>
+        </div>
+        <div class="post-footer-container">
+          <div class="post-footer">
+            <span class="post-footer-description"
+              >This is my first photo to feed, this is a multi line comment for
+              testing purposes. I am learning to code with
+              <span class="inside-span">#BreatheCodeByJhoan</span></span
+            >
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
